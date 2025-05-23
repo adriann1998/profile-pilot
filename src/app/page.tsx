b156717bf95f67f1855ac1c188deefa52f98ae5f
adriann1998 <angkajaya98@gmail.com>
2025-05-23 15:13:56 +1000

    progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,151 @@
+'use client';
 import Image from "next/image";
+import {
+  InboxIcon,
+  PhoneIcon,
+  MapPinIcon,
+  CircleStackIcon,
+  PaintBrushIcon,
+  Cog6ToothIcon,
+  ServerStackIcon,
+  ArrowDownTrayIcon,
+} from '@heroicons/react/24/solid';
+
+const profile = {
+  firstName: 'Adrian',
+  lastName: 'Angkajaya',
+  aboutMe: `I am a self-driven Full Stack Developer with a proven track record building robust front-end applications, APIs, and databases. I am language-agnostic and has a strong interest in learning new technologies. I am passionate about building scalable and maintainable applications, and I am always looking for tech-implementation I can do to improve my client's organisation.`,
+  jobTitle: 'Full Stack Developer',
+  email: 'angkajaya98@gmail.com',
+  phone: '+61 411 180 990',
+  location: 'Melbourne, Australia',
+  imageProfileUrl: '/static/images/profile.jpg',
+  imageAlt: 'Adrian Angkajaya',
+  socials: {
+    github: 'https://github.com/adriann1998',
+    linkedIn: 'https://www.linkedin.com/in/adrian-angkajaya-aa7641194/',
+  },
+  services: [
+    {
+      name: 'Front-end',
+      icon: <PaintBrushIcon className="size-6" />,
+    },
+    {
+      name: 'Back-end',
+      icon: <Cog6ToothIcon className="size-6" />,
+    },
+    {
+      name: 'Database',
+      icon: <CircleStackIcon className="size-6" />,
+    },
+    {
+      name: 'CI/CD pipeline',
+      icon: <CircleStackIcon className="size-6" />,
+    },
+    {
+      name: 'Server',
+      icon: <ServerStackIcon className="size-6" />,
+    }
+  ],
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+  return (
+    <div className="flex justify-center">
+      <div className="flex min-h-screen flex-col items-center justify-between p-16 w-full">
+        <div className="grid grid-cols-3 gap-10 w-full">
+          <div className="flex flex-col items-center">
+            {/* image, name, job title */}
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              className="rounded-full mb-4"
+              src={profile.imageProfileUrl}
+              alt='profile'
+              width={150}
+              height={150}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <h1 className="mb-4 font-bold text-4xl">{profile.firstName} {profile.lastName}</h1>
+            <p className="mb-8 text-xl font-normal text-gray-500">{profile.jobTitle}</p>
+
+            {/* profile section */}
+            <div className="flex flex-col items-start w-full gap-4 mb-12">
+              <div className="text-left w-full flex justify-start border-b-2 pb-5 border-gray-500">
+                <InboxIcon className="size-6 mr-4 cursor-pointer" onClick={() => window.location.href = `mailto:${profile.email}`}/>
+                <p className="text-xl">{profile.email}</p>
+              </div>
+              <div className="text-left w-full flex justify-start border-b-2 pb-5 border-gray-500">
+                <PhoneIcon className="size-6 mr-4 cursor-pointer" onClick={() => window.location.href = `tel:${profile.phone}`}/>
+                <p className="text-xl">{profile.phone}</p>
+              </div>
+              <div className="text-left w-full flex justify-start border-b-2 pb-5 border-gray-500">
+                <MapPinIcon className="size-6 mr-4 cursor-pointer" />
+                <p className="text-xl">{profile.location}</p>
+              </div>
+            </div>
+
+            {/* download CV */}
+            <div className="flex items-center gap-4 mb-12">
+              <button className="flex gap-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => alert('Download CV')}>
+                <ArrowDownTrayIcon className="size-6 mr-2" />
+                <span className="text-xl">Download CV</span>
+              </button>
+            </div>
+
+            {/* socials */}
+            <div className="flex items-center gap-4 mb-12">
+              {profile.socials.github && (
+                <Image
+                  className="rounded-full mb-4"
+                  src='/static/icons/github-mark-white.svg'
+                  alt='Github'
+                  width={50}
+                  height={50}
+                  onClick={() => window.open(profile.socials.github, '_blank')}
+                />
+              )}
+              {profile.socials.linkedIn && (
+                <Image
+                  className="rounded-full mb-4"
+                  src='/static/icons/linkedin.svg'
+                  alt='LinkedIn'
+                  width={50}
+                  height={50}
+                  onClick={() => window.open(profile.socials.linkedIn, '_blank')}
+                />
+              )}
+            </div>
+          </div>
+
+          <div>
+            {/* about me */}
+            <div className="flex flex-col items-center mb-16">
+              <h1 className="mb-4 font-bold text-3xl mb-4">About me</h1>
+              <p className="font-normal text-lg mb-4">{profile.aboutMe}</p>
+              <p className="font-normal text-lg mb-2 align-left w-full">As as {profile.jobTitle}, I develop and maintain:</p>
+              <div className="grid grid-cols-4 gap-6 w-full">
+                {profile.services.map((service, index) => (
+                  <div className="w-full bg-gray-800 rounded-xl flex flex-col items-center justify-center p-2 shadow-lg" key={`service-${index}`}>
+                    <div className="mb-2">
+                      {service.icon}
+                    </div>
+                    <p className="font-normal text-sm">{service.name}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+            
+            {/* work experience */}
+            <div className="flex flex-col items-center">
+              <h1 className="font-bold text-3xl mb-4">Work experience</h1>
+              <div className="w-full">
+                <p className="font-normal text-xl mb-4">Mid-leve Full Stack Developer</p>
+              </div>
+
+            </div>
+          </div>
+          <div>03</div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
